fae09c7f74aaea0c99df06728f91edd05777dac7
Oceanic Fisheries Programme <coreyc@spc.int>
2012-02-29 11:37:47 +1100

    Initial checkin

---
+++ OfpDatabaseUtilities/PreDeployScript.sql
@@ -0,0 +1,46 @@
+﻿
+/* Drop the function if it already exists (FT is Assembly table-valued function, FS is Assembly scalar-valued function) */
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FnSplitString]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [dbo].[FnSplitString]
+GO
+
+/* Drop the function if it already exists */
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FnSplitDelimitedString]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [dbo].[FnSplitDelimitedString]
+GO
+
+/* Drop the function if it already exists */
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[dbo].[FnSplitNumberString]') AND type in (N'FN', N'IF', N'TF', N'FS', N'FT'))
+DROP FUNCTION [dbo].[FnSplitNumberString]
+GO
+
+/* Drop the assembly if it already exists */
+IF EXISTS (SELECT [name] FROM sys.assemblies WHERE [name] = N'OfpDatabaseUtilities')
+DROP ASSEMBLY OfpDatabaseUtilities with NO DEPENDENTS;
+GO
+
+/* (Re)load the assembly */
+/*  NOTE:  This assembly works within the database and so doesn't need any special permissions */
+CREATE Assembly OfpDatabaseUtilities From
+'C:\Users\coreyc\Documents\Visual Studio 2010\Projects\OfpDatabaseUtilities\OfpDatabaseUtilities\bin\Debug\OfpDatabaseUtilities.dll'
+GO
+
+-- FnSplitNumberString splits a string into integer values with the default delimiter (,)
+CREATE FUNCTION [dbo].[FnSplitNumberString](@value nvarchar(max))
+RETURNS TABLE (item int)
+AS EXTERNAL NAME [OfpDatabaseUtilities].[Spc.Ofp.UserDefinedFunctions].[SplitStringIntoNumbers]
+GO
+
+-- Sample execution for FnSplitNumberString:
+-- SELECT * FROM [master].[dbo].[FnSplitNumberString] ('1,2,3,4,5,x')
+-- returns 1,2,3,4,5
+
+CREATE FUNCTION [dbo].[FnSplitDelimitedString](@value nvarchar(max), @delimiter nvarchar(255))
+RETURNS TABLE (item nvarchar(max))
+AS EXTERNAL NAME [OfpDatabaseUtilities].[Spc.Ofp.UserDefinedFunctions].[SplitDelimitedString]
+GO
+
+CREATE FUNCTION [dbo].[FnSplitString](@value nvarchar(max))
+RETURNS TABLE (item nvarchar(max))
+AS EXTERNAL NAME [OfpDatabaseUtilities].[Spc.Ofp.UserDefinedFunctions].[SplitString]
+GO
---
+++ OfpDatabaseUtilities/SplitString.cs
@@ -0,0 +1,93 @@
+﻿// <copyright file="SplitString.cs" company="Secretariat of the Pacific Community">
+//  Copyright 2012 Secretariat of the Pacific Community
+// </copyright>
+[assembly: System.CLSCompliant(true)]
+namespace Spc.Ofp
+{
+    /*
+     * This file is part of TUBS.
+     *
+     * TUBS is free software: you can redistribute it and/or modify
+     * it under the terms of the GNU Affero General Public License as published by
+     * the Free Software Foundation, either version 3 of the License, or
+     * (at your option) any later version.
+     *  
+     * TUBS is distributed in the hope that it will be useful,
+     * but WITHOUT ANY WARRANTY; without even the implied warranty of
+     * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     * GNU Affero General Public License for more details.
+     *  
+     * You should have received a copy of the GNU Affero General Public License
+     * along with TUBS.  If not, see <http://www.gnu.org/licenses/>.
+     */
+    using System;
+    using System.Data;
+    using System.Data.SqlClient;
+    using System.Data.SqlTypes;
+    using Microsoft.SqlServer.Server;
+    using System.Collections;
+
+    /// <summary>
+    /// This partial class holds String related functions.
+    /// </summary>
+    public partial class UserDefinedFunctions
+    {
+        private static readonly char[] DefaultDelimiter = { ',' };
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.None, FillRowMethodName = "FillStringRow", TableDefinition = "item nvarchar(max)")]
+        public static IEnumerable SplitDelimitedString(SqlString value, SqlString delimiter)
+        {
+            if (value.IsNull)
+            {
+                // Return an empty collection
+                return new string[] { };
+            }
+
+            // Assume this is what the user wants
+            if (delimiter.IsNull)
+            {
+                delimiter = new SqlString(",");
+            }
+
+            string clrString = value.ToString();
+            char[] clrDelimiter = delimiter.ToString().ToCharArray();
+            return clrString.Split(clrDelimiter);
+        }
+
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.None, FillRowMethodName = "FillStringRow", TableDefinition = "item nvarchar(max)")]
+        public static IEnumerable SplitString(SqlString value)
+        {
+            return SplitDelimitedString(value, new SqlString(","));
+        }
+
+        [Microsoft.SqlServer.Server.SqlFunction(DataAccess = DataAccessKind.None, FillRowMethodName = "FillNumberRow", TableDefinition = "item int")]
+        public static IEnumerable SplitStringIntoNumbers(SqlString value)
+        {
+            if (value.IsNull)
+            {
+                yield break;
+            }
+
+            foreach (string s in value.ToString().Split(DefaultDelimiter))
+            {
+                int number;
+                if (Int32.TryParse(s, out number))
+                {
+                    yield return number;
+                }
+            }            
+        }
+
+        public static void FillNumberRow(Object obj, out SqlInt32 item)
+        {
+            item = new SqlInt32((Int32)obj);
+        }
+
+        public static void FillStringRow(Object obj, out SqlString item)
+        {
+            item = new SqlString((String)obj);
+        }
+    }
+}
+
